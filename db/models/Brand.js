6b86e5efa04bde44e1369e8c5f07e4337f1ac4dc
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-02-15 03:43:42 -0300

    Modelo Brand listo

---
+++ db/models/Brand.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Brand";
+    let cols = {
+        id: {
+            type: dataTypes.INT(10).UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: dataTypes.STRING,
+            allowNull: false
+        }
+    };
+    let config = {
+        timestamps: true,
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+        deletedAt: "deleted_at"
+    };
+
+    const Brand = sequelize.define(alias, cols, config);
+
+    Brand.associate = function(models){
+        Brand.hasMany(models.Product, {
+            as: "products",
+            foreignKey: "brand_id"
+        })
+    }
+    return Brand
+}
