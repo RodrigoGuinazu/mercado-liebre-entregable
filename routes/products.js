5a230f2199a2574a3e91d58003d74354934e5f4c
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-02-15 21:43:02 -0300

    Se modifico la ruta para que la edicion sea por PATCH

--- routes/products.js
+++ routes/products.js
@@ -18,7 +18,7 @@ router.get('/:id', productsController.detail);
 
 /*** EDIT ONE PRODUCT ***/ 
 router.get('/:id/edit', productsController.edit); 
-router.put('/:id', productsController.update); 
+router.patch('/:id', productsController.update); 
 
 
 /*** DELETE ONE PRODUCT ***/ 
